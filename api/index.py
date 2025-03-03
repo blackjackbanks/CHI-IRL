1c78fd21f7c369dc023701b9480dde0bd56abfc8
Arliss <arliss.banks@gmail.com>
2025-03-02 22:37:57 -0600

    flask update

--- api/index.py
+++ api/index.py
@@ -1,25 +1,13 @@
 import sys
 import os
-import importlib.util
 
 # Get the absolute path of the project root
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0, project_root)
 
-# Dynamically import the create_app function
-def import_from_path(module_name, file_path):
-    spec = importlib.util.spec_from_file_location(module_name, file_path)
-    module = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(module)
-    return module
-
 # Import create_app from app.py
-app_module = import_from_path('app', os.path.join(project_root, 'app.py'))
-create_app = app_module.create_app
-
-# Import request from flask
-flask_module = import_from_path('flask', os.path.join(project_root, 'flask.py'))
-request = flask_module.request
+from app import create_app
+from flask import request
 
 # Create the Flask app
 app = create_app()
